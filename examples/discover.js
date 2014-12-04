74d69fcd73e5a48678ea816b6439c0b98edc5357
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-12-04 11:05:38 -0800

    emit device when found

--- examples/discover.js
+++ examples/discover.js
@@ -3,11 +3,16 @@
 var DeviceScanner = require('../').DeviceScanner;
 
 var scanner = new DeviceScanner();
+scanner.on('device', function (device) {
+  console.log('found:', device.name);
+});
+console.log('will start scanner');
 scanner.start();
 
 setTimeout(function () {
   console.log('will stop scanner');
   scanner.stop();
 
-  console.log('devices', scanner.devices);
-}, 30 * 1000);
+  var deviceCount = scanner.devices.length;
+  console.log('scanner found %s device%s', deviceCount, deviceCount !== 1 ? 's' : '');
+}, 60 * 1000);
