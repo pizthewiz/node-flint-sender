5e3732d74d4b7537fc9175711d529725ab0c0743
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-12-16 15:12:14 -0800

    emit events on system control and add example

---
+++ examples/control.js
@@ -0,0 +1,31 @@
+/* jshint node:true */
+'use strict';
+
+var DeviceManager = require('../').DeviceManager;
+
+var device = {
+  applicationUrl: 'http://localhost:9431/apps',
+  name: 'MatchStick_MAC_a474',
+  model: 'MatchStick'
+};
+var manager = manager = new DeviceManager(device);
+
+manager.on('volume', function (value) {
+  console.log('volume:', value);
+});
+manager.on('muted', function (value) {
+  console.log('muted:', value);
+});
+
+manager.getVolume();
+var value = Math.random();
+manager.setVolume(value);
+manager.setVolume(1.0);
+
+manager.getMuted();
+manager.setMuted(true);
+manager.setMuted(false);
+
+setTimeout(function () {
+  process.exit(0);
+}, 1 * 1000);
