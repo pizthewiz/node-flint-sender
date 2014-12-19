99737f768e9b166e3c8964eb56366d5e4240bff6
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-12-18 17:51:58 -0800

    add basic message channel support

---
+++ examples/audio-visualizer.js
@@ -0,0 +1,61 @@
+/* jshint node:true */
+'use strict';
+
+// based on the sender side of https://github.com/openflint/audio-visualizer-sample
+
+var DeviceManager = require('../').DeviceManager;
+var util = require('util');
+
+var appID = '~audio-visualizer';
+var appURL = 'http://openflint.github.io/audio-visualizer-sample/receiver_app/index.html';
+
+var device = {
+  applicationUrl: 'http://localhost:9431/apps',
+  name: 'MatchStick_MAC_a474',
+  model: 'MatchStick'
+};
+var manager = manager = new DeviceManager(device);
+manager.appID = appID;
+
+manager.on('launch', function () {
+  // quit after 30 seconds
+  setTimeout(function () {
+    manager.quitApp();
+  }, 30 * 1000);
+});
+manager.on('quit', function () {
+  console.log('app quit');
+  process.exit(0);
+});
+manager.on('error', function (err) {
+  console.error('ERROR - DeviceManager failure:', err);
+  process.exit(1);
+});
+manager.on('channel', function (channel) {
+  channel.on('open', function () {
+    console.log('channel opened');
+
+    var data = JSON.stringify({
+      'type': 'PLAY',
+      'effect': 'wave',
+      'url': 'http://openflint.github.io/audio-visualizer-sample/receiver_app/audio/EMDCR.ogg'
+    });
+    channel.send(data, function (err) {
+      if (err) {
+        console.error('ERROR - channel send failed:', util.inspect(err));
+        return;
+      }
+    });
+  });
+  channel.on('close', function () {
+    console.log('channel closed');
+  });
+  channel.on('error', function (err) {
+    console.error('ERROR - channel error:', util.inspect(err));
+  });
+  channel.on('message', function (data) {
+    console.log('received message:', util.inspect(data));
+  });
+});
+
+manager.launchApp(appURL, {messageChannel: true});
