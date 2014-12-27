aa1f631b4e1dfe5fdb9e63a1bbc40c1c4b031a1d
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-12-27 11:42:35 -0800

    print application URL of discovered device

--- examples/discover.js
+++ examples/discover.js
@@ -1,14 +1,16 @@
+/* jshint node:true */
 'use strict';
 
 var DeviceScanner = require('../').DeviceScanner;
 
 var scanner = new DeviceScanner();
 scanner.on('device', function (device) {
-  console.log('found:', device.name);
+  console.log('found \'%s\' with application URL: %s', device.name, device.applicationUrl);
 });
 console.log('will start scanner');
 scanner.start();
 
+// quit after 60 seconds
 setTimeout(function () {
   console.log('will stop scanner');
   scanner.stop();
