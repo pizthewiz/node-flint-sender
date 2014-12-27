1d76bba55ce31fd8445fbd2cbbb9898c3d057315
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-12-26 19:18:22 -0800

    use remote media player

--- examples/player.js
+++ examples/player.js
@@ -4,6 +4,9 @@
 // playback via the default flint player https://github.com/openflint/flint-player
 
 var DeviceManager = require('../').DeviceManager;
+var RemoteMediaPlayer = require('../').RemoteMediaPlayer;
+
+var util = require('util');
 
 var appID = '~flintplayer';
 var appURL = 'http://openflint.github.io/flint-player/player.html';
@@ -21,24 +24,13 @@ manager.on('error', function (err) {
   process.exit(1);
 });
 manager.on('channel', function (channel) {
+  var player = new RemoteMediaPlayer(channel);
+  player.on('message', function (data) {
+    console.log('received player message:', util.inspect(data, {depth: 5}));
+  });
+
   channel.on('open', function () {
-    // NB - weird double stringify for the default player message format
-    var data = JSON.stringify({
-      namespace: 'urn:flint:org.openflint.fling.media',
-      payload: JSON.stringify({
-        type: 'LOAD',
-        requestId: 'requestId-2',
-        media: {
-          contentId: 'http://fling.matchstick.tv/droidream/samples/BigBuckBunny.mp4',
-          contentType: 'video/mp4',
-          metadata: {
-            title: '',
-            subtitle: ''
-          }
-        }
-      })
-    });
-    channel.send(data, function (err) {
+   player.load('http://fling.matchstick.tv/droidream/samples/BigBuckBunny.mp4', function (err) {
       if (err) {
         console.error('ERROR - failed to load media:', err);
         return;
@@ -53,12 +45,6 @@ manager.on('channel', function (channel) {
   });
   channel.on('error', function (err) {
     console.error('ERROR - MessageChannel failure:', err);
-  });
-  channel.on('message', function (data) {
-    // NB - weird double parse given the default player message format
-    data = JSON.parse(data);
-    data.payload = JSON.parse(data.payload);
-    console.log('received message:', data);
   });
 });
 
